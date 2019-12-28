61a38ef34b909f05d2427ff9e422fc2d951484b3
zhoupanyin <zpy2463741756@163.com>
2019-12-28 15:44:55 +0800

    Fix:修改1

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,39 @@ export default {
     return{}
   }
 }
+// promise.all电力示例
+//  getData(index) {
+//             let _this = this;
+//             return new Promise((resolve, reject) => {
+//                 _this.$refs["modelForm" + index].validate(vaild => {
+//                     vaild ? resolve(true) : reject(true);
+//                 })
+//             })
+
+//         },
+
+//   checkValidateForm() {
+//             return new Promise((resolve, reject) => {
+//                 let pArr = [];
+//                 for (let index = 0; index < 3; index++) {
+//                     pArr.push(this.getData(index))
+//                 };
+//                 Promise.all(pArr).then(res => {
+//                     resolve(true)
+//                 }).catch(err => {
+//                     reject(true);
+//                 })
+//             })
+//         },
+//   checkValidCoal() {
+//             let canSubmit = true;
+//             let promise = this.checkValidateForm().then(res => {
+//                 return Promise.resolve(true)
+//             }).catch(err => {
+//                 return Promise.resolve(false)
+//             })
+//             return promise
+//         },
 </script>
 
 <style>
